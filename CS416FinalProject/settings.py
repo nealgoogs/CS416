d579d77bce2a27246bae8420a0304ec64c464f18
Niall <ngeoghegan@my.ccsu.edu>
2023-12-03 09:39:41 -0500

    Changes

--- CS416FinalProject/settings.py
+++ CS416FinalProject/settings.py
@@ -17,7 +17,6 @@ import authapp.apps
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -28,7 +27,6 @@ SECRET_KEY = 'django-insecure-tq--%k@%q%i7jj-2g*1i4$lswd6k#0p@lykw(duqi7)e%2uq!*
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -44,7 +42,7 @@ INSTALLED_APPS = [
     'TicketMaster.apps.TicketmasterConfig',
     'authapp.apps.AuthappConfig',
     'userInfo.apps.UserinfoConfig'
-    ]
+]
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
@@ -80,7 +78,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'CS416FinalProject.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -90,7 +87,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -110,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -122,18 +117,15 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
 
-#STATICFILES_DIRS = ['static']
-STATICFILES_DIRS = [BASE_DIR / 'static']
-
+# STATICFILES_DIRS = ['static']
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
